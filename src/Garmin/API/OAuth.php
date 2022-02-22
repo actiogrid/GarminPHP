2ba5d20fe971e3d1eb5d0a47e0db04995783370d
James Van Hinsbergh <jvanhins@gmail.com>
2022-02-22 09:50:58 +0000

    Fix function delaration from oauth signature update

--- src/Garmin/API/OAuth.php
+++ src/Garmin/API/OAuth.php
@@ -68,9 +68,11 @@ class OAuth extends Server
      * identifier or an object instance.
      *
      * @param TemporaryCredentials|string $temporaryIdentifier
+     * @param array                       $options
+     * 
      * @return string
      */
-    public function getAuthorizationUrl($temporaryIdentifier): string
+    public function getAuthorizationUrl($temporaryIdentifier, array $options = [])
     {
         // Somebody can pass through an instance of temporary
         // credentials and we'll extract the identifier from there.
